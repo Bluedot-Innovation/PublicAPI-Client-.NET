aca8d4606dbc3c7c7ff3d9d7c7adfd564f080cb5
Balendran <password>
2014-12-19 18:23:18 +1100

    Added call examples in main

--- BluedotOpenApiClient/Program.cs
+++ BluedotOpenApiClient/Program.cs
@@ -13,7 +13,7 @@ using BluedotPublicApiClient.checkinactivityclient;
 
 /**
  * @author Bluedot Innovation
- * Add application java client demonstrates adding an application to your Bluedot backend using Web Api.
+ * Main program to execute the various client examples
  */
 
 namespace BluedotPublicApiClient
@@ -22,82 +22,78 @@ namespace BluedotPublicApiClient
     {
         static void Main(string[] args)
         {
-            FenceBulkLoader fenceBulkLoader = new FenceBulkLoader();
-            //fenceBulkLoader.loadFences();
 
-
+            /*Get a list of applications for a customer*/
+            GetAllApplications appsClient = new GetAllApplications();
+            appsClient.getAllApplicationsForCustomer();
+            
             /*Add an application to the Bluedot backend*/
             CreateApplication bdApplicationClient = new CreateApplication();
             bdApplicationClient.create();
-
+            
             /*Get a list of zone id and name for a given customer*/
             GetAllZones zones = new GetAllZones();
-            //zones.getAllZonesForCustomer();
+            zones.getAllZonesForCustomer();
 
-            CreateZone addZone = new CreateZone();
-            //addZone.add();
-            
-            //GetAllApplications apps = new GetAllApplications();
+            /*Create a new zone*/
+            CreateZone createZoneClient = new CreateZone();
+            createZoneClient.create();
+          
 
-            //apps.getAllApplicationsForCustomer();
-            
+            /*Create a geo-fence to an existing zone*/
+            BDAddFenceClient fenceClient = new BDAddFenceClient();
+            fenceClient.addFence(); //Fence client has JSON for various shapes for your reference
 
-            /*Add a fence to an existing zone*/
-             BDAddFenceClient fenceClient = new BDAddFenceClient();
-             fenceClient.addFence(); //Fence client has JSON for various shapes for your reference
+            /*Update an existing geo-fence*/
+            BDUpdateFenceClient updateFenceClient = new BDUpdateFenceClient();
+            updateFenceClient.updateFence();
 
-            //BDUpdateFenceClient updateFenceClient = new BDUpdateFenceClient();
-            //updateFenceClient.updateFence();
 
-            /*Add actions to an existing zone*/
-            CreateAllActions actionClient = new CreateAllActions();
-            //actionClient.addApplicationAction();
-            //actionClient.addMessageAction();
-            //actionClient.addSoundAction();
-            //actionClient.addURLAction();
+            CreateMessageAction messageActionClient = new CreateMessageAction();
+            messageActionClient.create();
 
-            CreateMessageAction addMessageAction = new CreateMessageAction();
-            addMessageAction.add();
+            CreateURLAction urlActionClient = new CreateURLAction();
+            urlActionClient.create();
 
-            CreateURLAction urlAction = new CreateURLAction();
-            //urlAction.add();
+            CreateVibrationAction bdVibrationActionClient = new CreateVibrationAction();
+            bdVibrationActionClient.create();
 
-            CreateVibrationAction bdVibrationAction = new CreateVibrationAction();
-            bdVibrationAction.add();
-
-            CreateSoundAction bdAddSoundAction = new CreateSoundAction();
-           bdAddSoundAction.add();
+            CreateSoundAction bdSoundActionClient = new CreateSoundAction();
+            bdSoundActionClient.create();
 
             /*Add conditions to actions*/
             CreateAllActionsWithAllConditions bdConditionsClient = new CreateAllActionsWithAllConditions();
-            //bdConditionsClient.addConditionsToMessageAction();
+            bdConditionsClient.addConditionsToMessageAction();
 
 
-            CreateMessageActionWithConditions bdAddMessageActionWithConditions = new CreateMessageActionWithConditions();
-            //bdAddMessageActionWithConditions.addActionWithConditions();
+            CreateMessageActionWithConditions bdCreateMessageActionWithConditions = new CreateMessageActionWithConditions();
+            bdCreateMessageActionWithConditions.createActionWithConditions();
             
-            CreateApplicationActionWithConditions bdApplicationActionWithConditions = new CreateApplicationActionWithConditions();
-            //bdApplicationActionWithConditions.addActionWithConditions();
+            CreateApplicationActionWithConditions bdCreateApplicationActionWithConditions = new CreateApplicationActionWithConditions();
+            bdCreateApplicationActionWithConditions.createActionWithConditions();
 
-            //bdConditionsClient.addConditionsToVibrationAction();
+            CreateSoundActionWithConditions bdCreateSoundActionWithConditions = new CreateSoundActionWithConditions();
+            bdCreateSoundActionWithConditions.createActionWithConditions();
 
-            CreateSoundActionWithConditions bdAddSoundActionWithConditions = new CreateSoundActionWithConditions();
-            //bdAddSoundActionWithConditions.addActionWithConditions();
+            CreateApplicationActionWithConditions bdCreateApplicationActionWithCondtions = new CreateApplicationActionWithConditions();
+            bdCreateApplicationActionWithCondtions.createActionWithConditions();
 
-            CreateApplicationActionWithConditions bdApplicationActionWithCondtions = new CreateApplicationActionWithConditions();
-            //bdApplicationActionWithCondtions.addActionWithConditions();
+            CreateVibrationActionWithConditions bdCreateVibrationActionWithConditions = new CreateVibrationActionWithConditions();
+            bdCreateVibrationActionWithConditions.createActionWithConditions();
 
-            CreateVibrationActionWithConditions bdVibrationActionWithConditions = new CreateVibrationActionWithConditions();
-            //bdVibrationActionWithConditions.addActionWithConditions();
+            CreateURLActionWithConditions bdCreateURLActionWithConditions = new CreateURLActionWithConditions();
+            bdCreateURLActionWithConditions.createActionWithConditions();
 
-            CreateURLActionWithConditions bdURLActionWithConditions = new CreateURLActionWithConditions();
-            //bdURLActionWithConditions.addActionWithConditions();
+
             /*Get Checkin activities*/
             GetCheckinActivities bdCheckinActivity = new GetCheckinActivities();
-            //bdCheckinActivity.getAllCheckInActivitesByZone();
-            //bdCheckinActivity.getCheckInActivitiesByZoneAndDateRange();
+            bdCheckinActivity.getAllCheckInActivitesByZone();
+            bdCheckinActivity.getCheckInActivitiesByZoneAndDateRange();
 
             /*Create a zone, fences, actions and conditions in one call*/
+            FenceBulkLoader fenceBulkLoader = new FenceBulkLoader();
+            //fenceBulkLoader.loadFences();
+
 
         }
     }
